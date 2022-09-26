23be572d8f955af3681744ab7a606637e744d4af
Musti <guldag.mustafaa@gmail.com>
2022-09-26 11:26:23 +0200

    final version

--- src/container/ShippingPage/ShippingPage.js
+++ src/container/ShippingPage/ShippingPage.js
@@ -1,67 +1,77 @@
-import React from "react";
+import { React, useState } from "react";
 import "@vtmn/icons/dist/vitamix/font/vitamix.css";
+import Alert from 'react-bootstrap/Alert';
 import { VtmnIcon } from "@vtmn/react";
 import { FaBoxOpen } from "react-icons/fa";
 import { GiWorld } from "react-icons/gi";
+import { HiShoppingCart } from "react-icons/hi";
 import "./ShippingPage.css";
 import products from "../../products.json";
 
 export default function ShippingPage() {
-  const showTheDate = () => {
-    const currentDate = new Date();
-    console.log(currentDate);
+
+
+
+  const [selectedOption, setSelectedOption] = useState(0);
+
+  const handleOption = (id) => {
+    setSelectedOption(id);
   };
 
-  showTheDate();
+  // const months = [
+  //   "January",
+  //   "February",
+  //   "March",
+  //   "April",
+  //   "May",
+  //   "June",
+  //   "July",
+  //   "August",
+  //   "September",
+  //   "October",
+  //   "November",
+  //   "December",
+  // ];
 
-  const months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-  ];
+  // const currentDate = new Date();
+  // let month = months[currentDate.getMonth()];
 
-  const currentDate = new Date();
-  let month = months[currentDate.getMonth()];
+  // const showTheDate = () => {
+  // const currentDate = new Date();
+  // console.log(currentDate);
+  // };
 
-  let productstotal = 0;
+
+  //We check the size of the Cart
+  let productsTotalSize = 0;
   products.forEach((product) => {
-    productstotal += product.size;
+    productsTotalSize += product.size;
   });
-  console.log(productstotal);
 
-  const checkShipping = (total) => {
+  //We check, how long shipping takes according to size of Cart
+  const checkShipping = (totalShippingSize) => {
     let shippingDay = 0;
-    if (total < 15) {
+    if (totalShippingSize < 15) {
       return shippingDay + 2;
-    } else if (15 <= total && total <= 20) {
+    } else if (15 <= totalShippingSize && totalShippingSize <= 20) {
       return shippingDay + 4;
     } else {
       return shippingDay + 6;
     }
   };
 
-  let finalShippingDay = checkShipping(productstotal);
-  console.log(finalShippingDay);
-  console.log(checkShipping(productstotal));
+  //We found how long it takes shipping
+  let finalShippingAmount = checkShipping(productsTotalSize);
 
   const christmasDate = new Date("December 24, 2022 00:00:00");
-  //24 aralik - shippingDay bize en gec order vermesi gereken tarihi verir.
-  const compareForChristmas = (finalShippingDay) => {
+  //24 december - finalShippingAmount gives us the last Day Of Order.
+  const compareForChristmas = (finalShippingAmount) => {
     const christmasDate = new Date("December 24, 2022 00:00:00");
-    console.log(christmasDate.getDate() - finalShippingDay);
 
-    return christmasDate.getDate() - finalShippingDay;
+
+    return christmasDate.getDate() - finalShippingAmount;
   };
-  compareForChristmas(finalShippingDay);
+
 
   return (
     <div className="shippingPageContainer">
@@ -74,50 +84,40 @@ export default function ShippingPage() {
       </div>
       <div className="options">
         <div
-          className="option"
-          onClick={() => {
-            console.log("id: ", 0);
-          }}
+          className={`option ${selectedOption === 0 ? "selectedOption" : ""}`}
+          onClick={() => handleOption(0)}
         >
           <VtmnIcon size={24} value="truck-line" variant="default" />
           <span>Házhozszállítás</span>
         </div>
 
         <div
-          className="option"
-          onClick={() => {
-            console.log("id: ", 1);
-          }}
+          className={`option ${selectedOption === 1 ? "selectedOption" : ""}`}
+          onClick={() => handleOption(1)}
         >
           <VtmnIcon size={24} value="store-line" variant="default" />
           <span>Áruházi átvétel</span>
         </div>
 
         <div
-          className="option"
-          onClick={() => {
-            console.log("id: ", 2);
-          }}
+          className={`option ${selectedOption === 2 ? "selectedOption" : ""}`}
+          onClick={() => handleOption(2)}
         >
           <VtmnIcon size={24} value="flashlight-line" variant="default" />
           <span>Aznapi kisátílás</span>
         </div>
 
         <div
-          className="option"
-          onClick={() => {
-            console.log("id: ", 3);
-          }}
+          className={`option ${selectedOption === 3 ? "selectedOption" : ""}`}
+          onClick={() => handleOption(3)}
         >
           <VtmnIcon size={24} value="truck-fill" variant="default" />
           <span>1 munkanapos kisátílás</span>
         </div>
 
         <div
-          className="option"
-          onClick={() => {
-            console.log("id: ", 4);
-          }}
+          className={`option ${selectedOption === 4 ? "selectedOption" : ""}`}
+          onClick={() => handleOption(4)}
         >
           <VtmnIcon size={24} value="store-fill" variant="default" />
           <span>1 órás áruházi átvétel</span>
@@ -134,14 +134,14 @@ export default function ShippingPage() {
       </div>
 
       <div className="deliveryOptions">
-        <div className="deliveryOption">
+        <div className="deliveryOption" style={{ opacity: '0.6' }}>
           <div className="deliveryOptionHeader">
             <GiWorld size={36} />
             <span>KÜLFÖLDI KÉSZLETBŐL</span>
           </div>
           <div className="deliveryOptionDate">
             <span className="deliveryOptionDateFirstSpan">18</span>
-            <span>{month.substring(0, 3).toUpperCase()}</span>
+            <span>DEC</span>
           </div>
         </div>
 
@@ -154,7 +154,8 @@ export default function ShippingPage() {
             <span className="deliveryOptionDateFirstSpan">
               {christmasDate.getDate() - 2}
             </span>
-            <span>{month.substring(0, 3).toUpperCase()}</span>
+            <span>DEC</span>
+            {/* <span>{month.substring(0, 3).toUpperCase()}</span> */}
           </div>
         </div>
 
@@ -168,7 +169,6 @@ export default function ShippingPage() {
               {christmasDate.getDate() - 4}
             </span>
             <span>DEC</span>
-            {/* <span>{month.substring(0, 3).toUpperCase()}</span> */}
           </div>
         </div>
 
@@ -181,20 +181,20 @@ export default function ShippingPage() {
             <span className="deliveryOptionDateFirstSpan">
               {christmasDate.getDate() - 6}
             </span>
-            <span>{month.substring(0, 3).toUpperCase()}</span>
+            <span>DEC</span>
           </div>
         </div>
 
         <div className="deliveryOption">
           <div className="deliveryOptionHeaderChristmas">
-            <FaBoxOpen size={36} />
-            <span>YOUR FINAL SHIPPING</span>
+            <HiShoppingCart size={36} />
+            <span>YOUR FINAL ORDER</span>
           </div>
           <div className="deliveryOptionDateChristmas">
             <span className="deliveryOptionDateFirstSpan">
-              {compareForChristmas(finalShippingDay)}
+              {compareForChristmas(finalShippingAmount)}
             </span>
-            <span>{month.substring(0, 3).toUpperCase()}</span>
+            <span>DEC</span>
           </div>
         </div>
       </div>
